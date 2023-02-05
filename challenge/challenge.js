8a8baa45cb98e9fdac9712bfd412c7d56a33ee73
Alejandro Antelo <alfasant@protonmail.com>
2023-02-05 12:28:54 +0100

    Fixed clipping of circles

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -19,12 +19,12 @@ export function Circle(x, y, size, vx, vy, color) {
   }
   this.color = color ? color : this.randomColor()
   this.intersects = function (that) {
-    let x1 = this.x
-    let y1 = this.y
-    let r1 = this.size
-    let x2 = that.x
-    let y2 = that.y
-    let r2 = that.size
+    const x1 = this.x
+    const y1 = this.y
+    const r1 = this.size
+    const x2 = that.x
+    const y2 = that.y
+    const r2 = that.size
 
     const xNotIntersects = x2 > r1 + x1 || x1 > r2 + x2
     const yNotIntersects = y2 > r1 + y1 || y1 > r2 + y2
@@ -54,10 +54,28 @@ export function animate(arr) {
     circle.draw()
     circle.x += circle.vx
     circle.y += circle.vy
-    const yOverflows = circle.y > (canvas.height - circle.size) || circle.y < circle.size
-    const xOverflows = circle.x > (canvas.width - circle.size) || circle.x < circle.size 
-    if (yOverflows) circle.vy *= -1
-    if (xOverflows) circle.vx *= -1
+
+    const bottom = (canvas.height - circle.size) 
+    const top = circle.size
+    const right = (canvas.width - circle.size) 
+    const left = top
+
+    const bottomOverflows = circle.y > bottom
+    const topOverflows = circle.y < top
+    const yOverflows = topOverflows || bottomOverflows
+
+    const rightOverflows = circle.x > right
+    const leftOVerflows = circle.x < left
+    const xOverflows = rightOverflows || leftOVerflows
+
+    if (yOverflows) {
+      circle.y = bottomOverflows ? bottom : top
+      circle.vy *= -1
+    }
+    if (xOverflows) {
+      circle.x = rightOverflows ? right : left
+      circle.vx *= -1
+    }
 
   }
   requestAnimationFrame(() => { animate(arr) })
